daa9caccebb858b793dc2207252abffc77de48e8
Bryan Kegley <hi@bryankegley.me>
2020-10-30 09:14:28 -0500

    update dotfiles

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,7 @@ killall -q polybar
 # Wait until all processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+polybar left &
+polybar center &
+polybar right &
 
---
+++ polybar/scripts/audio-menu.sh
@@ -0,0 +1,8 @@
+sink_description=$(pactl list sinks | awk '
+/Description/ {$1=""; print $0}
+' | rofi -dmenu -theme $HOME/repos/personal/dotfiles/rofi/dropdown.rasi -yoffset 50 -xoffset -20 -location northeast)
+
+pactl list sinks | awk -v descpription="s*$sink_description" '
+  /^Sink/ {sink_id = $2}
+  $0 ~ description {print sink_id}
+' | sed s/\#//g | xargs pactl set-default-sink
